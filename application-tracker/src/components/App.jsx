390074a8ace2ba568cadbc945ef800303ab28bca
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-25 23:36:46 -0700

    made code spacing prettier

--- application-tracker/src/components/App.jsx
+++ application-tracker/src/components/App.jsx
@@ -5,65 +5,65 @@ import Job from "./Job";
 import TableHeader from "./TableHeader";
 
 function App() {
-  const [jobs, setJobs] = useState([]);
+    const [jobs, setJobs] = useState([]);
 
-  function addJob(newJob) {
-    setJobs(prevJobs => {
-      return [...prevJobs, newJob];
-    });
-  }
+    function addJob(newJob) {
+        setJobs(prevJobs => {
+            return [...prevJobs, newJob];
+        });
+    }
 
-  function addEditedJob(editedJob) {
-    setJobs(jobs.map((jobList, index) => {
-      if (index === editedJob.id) {
-        return jobList = editedJob;
-      } else {
-        return jobList;
-      }
-    }))
-  }
+    function addEditedJob(editedJob) {
+        setJobs(jobs.map((jobList, index) => {
+            if (index === editedJob.id) {
+                return jobList = editedJob;
+            } else {
+                return jobList;
+            }
+        }));
+    }
 
-  function editJob(id) {
-    setJobs(prevJobs => {
-      return prevJobs.filter((jobList, index) => {
-        return index === id;
-      });
-    });
-  }
+    function editJob(id) {
+        setJobs(prevJobs => {
+            return prevJobs.filter((jobList, index) => {
+                return index === id;
+            });
+        });
+    }
   
-  function deleteJob(id) {
-    setJobs(prevJobs => {
-      return prevJobs.filter((jobList, index) => {
-        return index !== id;
-      });
-    });
-  }
+    function deleteJob(id) {
+        setJobs(prevJobs => {
+            return prevJobs.filter((jobList, index) => {
+                return index !== id;
+            });
+        });
+    }
 
-  return (
-    <div>
-      <Header saveJob={addJob} />
-      <TableHeader />
-      <div className="content"> 
-        {jobs.map((jobList, index) => {
-          return (
-            <Job
-              key={index}
-              id={index}
-              title={jobList.title}
-              company={jobList.company}
-              location={jobList.location}
-              link={jobList.link}
-              removeJob={deleteJob}
-              saveJob={addJob}
-              updateJob={editJob}
-              saveEdit={addEditedJob}
-            />
-          );
-        })}
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header saveJob={addJob} />
+            <TableHeader />
+            <div className="content"> 
+                {jobs.map((jobList, index) => {
+                    return (
+                        <Job
+                            key={index}
+                            id={index}
+                            title={jobList.title}
+                            company={jobList.company}
+                            location={jobList.location}
+                            link={jobList.link}
+                            removeJob={deleteJob}
+                            saveJob={addJob}
+                            updateJob={editJob}
+                            saveEdit={addEditedJob}
+                        />
+                    );
+                })};
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
