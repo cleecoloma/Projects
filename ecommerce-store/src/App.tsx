c87c3294ef8378a422f23719c7007af7cb0d68f6
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-28 21:36:22 -0700

    added navbar

--- ecommerce-store/src/App.tsx
+++ ecommerce-store/src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
 function App() {
 
   return (
-    <h1>Hello world! I'm Chester. I want to be a Front-end Web Developer!</h1>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home /> }></Route>
+          <Route path="/store" element={<Store /> }></Route>
+          <Route path="/about" element={<About /> }></Route>
+        </Routes>
+      </Container>
+    </>
   )
 }
 
---
+++ ecommerce-store/src/components/Navbar.tsx
@@ -0,0 +1,59 @@
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar as NavbarBS,
+} from "react-bootstrap";
+import { NavLink } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          className="rounded-circle"
+          variant="outline-primary"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="24"
+            fill="blue"
+            class="bi bi-bag-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+          </svg>
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            8
+          </div>
+        </Button>
+      </Container>
+    </NavbarBS>
+  );
+}
+
+export default Navbar;
