d3c229c24deb2f1aa6d6d1f354bf3d30f8661796
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-29 22:38:06 -0700

    added store items as bootstrap cards

--- ecommerce-store/src/pages/Store.tsx
+++ ecommerce-store/src/pages/Store.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
 import axios from "axios";
 
 function Store() {
@@ -19,22 +20,30 @@ function Store() {
   },[])
 
   return (
-    <div>
+    <div className="item-list">
       {loading && (
         <div>
           <h1>Loading...</h1>
         </div>
       )}
       {data.map((product) => (
-        <div key={product.id} className="card">
-          <div>
-            <img src={product.image} alt="#" />
-          </div>
-          <div className="card-description">
-            <h6>{product.title}</h6>
-            <h6>{`Price: $${product.price}`}</h6>
-          </div>
-        </div>
+        // <div key={product.id} className="card">
+        //   <div>
+        //     <img src={product.image} alt="#" />
+        //   </div>
+        //   <div className="card-description">
+        //     <h6>{product.title}</h6>
+        //     <h6>{`Price: $${product.price}`}</h6>
+        //   </div>
+        // </div>
+        <Card className="card-item" style={{ width: "16em" }}>
+          <Card.Img variant="top" src={product.image} />
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>{product.description}</Card.Text>
+            <Card.Text>{product.price}</Card.Text>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
