ffd9512a29b571f2e10ac6bf4fe13678b0617470
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-03-29 22:17:21 -0700

    fetched products from fakestoreapi

--- ecommerce-store/src/pages/Store.tsx
+++ ecommerce-store/src/pages/Store.tsx
@@ -1,7 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function Store() {
-    return (
-        <h1>Store</h1>
-    )
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios({
+      method: "GET",
+      url: "https://fakestoreapi.com/products",
+    }).then(res => {
+      console.log(res.data)
+      setData(res.data)
+    })
+      .catch(e => console.log(e))
+    .finally(() => setLoading(false))
+  },[])
+
+  return (
+    <div>
+      {loading && (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      )}
+      {data.map((product) => (
+        <div key={product.id} className="card">
+          <div>
+            <img src={product.image} alt="#" />
+          </div>
+          <div className="card-description">
+            <h6>{product.title}</h6>
+            <h6>{`Price: $${product.price}`}</h6>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Store
+export default Store;
