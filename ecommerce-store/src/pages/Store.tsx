2644be931bd35115e5d86bbdb52f7ec990694855
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-03 20:47:17 -0700

    updated card info

--- ecommerce-store/src/pages/Store.tsx
+++ ecommerce-store/src/pages/Store.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
 import axios from "axios";
 import Pdescription from "../components/Pdescription";
-
+import formatCurrency from "../utilities/formatCurrency";
 
 function Store() {
   const [loading, setLoading] = useState(false);
@@ -13,13 +13,14 @@ function Store() {
     axios({
       method: "GET",
       url: "https://fakestoreapi.com/products",
-    }).then(res => {
-      console.log(res.data)
-      setData(res.data)
     })
-      .catch(e => console.log(e))
-    .finally(() => setLoading(false))
-  },[])
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <div className="item-list">
@@ -31,20 +32,20 @@ function Store() {
       {data.map((product) => (
         <Card
           className="card-item m-2"
-          style={{ width: "16em", position: "relative" }}
+          style={{ width: "22em", position: "relative" }}
         >
           <Card.Img
             style={{ height: "20rem" }}
             variant="top"
             src={product.image}
           />
-          <Card.Body style={{ height: "20rem" }}>
+          <Card.Body style={{ height: "18rem" }}>
             <Card.Title>{product.title}</Card.Title>
             <Card.Text>
               <Pdescription description={product.description} />
             </Card.Text>
-            <Card.Text style={{ bottom: "0.5rem", position: "absolute" }}>
-              {product.price}
+            <Card.Text>
+              <span>{formatCurrency(product.price)}</span>
             </Card.Text>
           </Card.Body>
         </Card>
