1271e378ede481a72d2dc207154b5c13cba10786
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-04 18:35:55 -0700

    added navbar

--- electronic-store/src/App.tsx
+++ electronic-store/src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./pages/Navbar";
 
 function App() {
   return (
-    <Container>
-      Hi
+    <Container className="mb-4">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
---
+++ electronic-store/src/pages/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Container, Navbar as NavbarBs } from "react-bootstrap";
+
+function Navbar() {
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+      <Container>
+        Navbar
+      </Container>
+    </NavbarBs>
+  )
+}
+
+export default Navbar
