a0637acf16e59982ac4864ef7656f17101d987b5
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-04 20:54:07 -0700

    added currency formatter and add to cart button

--- electronic-store/src/pages/Store.tsx
+++ electronic-store/src/pages/Store.tsx
@@ -5,7 +5,6 @@ import StoreItem from '../components/StoreItem';
 function Store() {
   return (
     <>
-      <div>Store</div>
       <Row md={2} xs={1} lg={3} className='g-3'>
         {storeItems.map((item) => (
           <Col key={item.id}><StoreItem {...item}/></Col>
