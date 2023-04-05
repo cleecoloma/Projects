ee388bce6c397426300decba733485e3232ffe07
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-04 18:24:12 -0700

    added home, about, and store pages

--- electronic-store/src/App.tsx
+++ electronic-store/src/App.tsx
@@ -3,8 +3,15 @@ import { Container } from "react-bootstrap"
 
 function App() {
   return (
-    <Container>Hi</Container>
-  )
+    <Container>
+      Hi
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App
