c1450f2e3be43b282c93616a55f4597e0fa66601
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-05 22:21:26 -0700

    added divs for bag stacks

--- electronic-store/src/components/ShoppingBag.tsx
+++ electronic-store/src/components/ShoppingBag.tsx
@@ -1,17 +1,25 @@
-import { Offcanvas } from "react-bootstrap"
+import { Offcanvas, Stack } from "react-bootstrap"
 import { useShoppingBag } from "../context/ShoppingBagContent";
+import BagItem from "./BagItem";
 
 type ShoppingBagProps = {
   isOpen: boolean;
 }
 
 function ShoppingBag({ isOpen } : ShoppingBagProps) {
-  const { closeBag } = useShoppingBag()
+  const { closeBag, bagItems } = useShoppingBag()
   return (
     <Offcanvas show={isOpen} onHide={closeBag} placement="end">
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Bag</Offcanvas.Title>
       </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {bagItems.map((item) => (
+            <BagItem key={item.id} {...item} />
+          ))}
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
   );
 }
