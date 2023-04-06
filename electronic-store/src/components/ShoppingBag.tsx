45d04cddc668d5186574577b27f33a729232af95
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-05 22:26:33 -0700

    finished bag canvas functionality

--- electronic-store/src/components/ShoppingBag.tsx
+++ electronic-store/src/components/ShoppingBag.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap"
 import { useShoppingBag } from "../context/ShoppingBagContent";
 import BagItem from "./BagItem";
+import formatCurrency from "../utilities/formatCurrency";
+import storeItems from "../data/items.json";
 
 type ShoppingBagProps = {
   isOpen: boolean;
@@ -18,6 +20,12 @@ function ShoppingBag({ isOpen } : ShoppingBagProps) {
           {bagItems.map((item) => (
             <BagItem key={item.id} {...item} />
           ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total {formatCurrency(bagItems.reduce((total, bagItem) => {
+              const item = storeItems.find((i) => i.id === bagItem.id)
+              return total + (item?.price || 0) * bagItem.quantity
+            }, 0))}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
