8640cca93d2655f6c5b04ed42c18272a1a18a6ff
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-18 17:35:43 -0700

    added store page to show all items

--- electronic-store/src/pages/Store.tsx
+++ electronic-store/src/pages/Store.tsx
@@ -1,7 +1,25 @@
-import { Col, Row } from 'react-bootstrap';
-import storeItems from "../data/items.json";
-import StoreItem from '../components/StoreItem';
+import { Col, Row } from "react-bootstrap";
 import StoreNavBar from "../components/StoreNavbar";
+import cpuItems from "../data/cpu.json";
+import gpuItems from "../data/gpu.json";
+import storageItems from "../data/storage.json";
+import memoryItems from "../data/memory.json";
+import motherboardItems from "../data/motherboard.json";
+import powerSupplyItems from "../data/power-supply.json";
+import caseItems from "../data/case.json";
+import coolingItems from "../data/cooling.json";
+import StoreItem from "../components/StoreItem";
+
+const storeItems = [
+  ...cpuItems,
+  ...gpuItems,
+  ...storageItems,
+  ...memoryItems,
+  ...motherboardItems,
+  ...powerSupplyItems,
+  ...caseItems,
+  ...coolingItems,
+];
 
 function Store() {
   return (
@@ -18,4 +36,4 @@ function Store() {
   );
 }
 
-export default Store
+export default Store;
