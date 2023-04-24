50586cd5541233913a575a95933d8d0e6796303f
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-23 21:46:15 -0700

    Added a brief homepage info statement

--- electronic-store/src/pages/Home.tsx
+++ electronic-store/src/pages/Home.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <p>
+        Welcome to our PC component e-commerce website! We offer a wide range of
+        high-quality computer components at competitive prices. From CPUs and
+        GPUs to motherboards and RAM, we have everything you need to build or
+        upgrade your PC. Our user-friendly interface makes it easy to navigate
+        and find the components you need, and our fast and reliable shipping
+        ensures that your order will arrive quickly. Shop with us today and take
+        your PC to the next level!
+      </p>
+    </div>
+  );
 }
 
 export default Home
