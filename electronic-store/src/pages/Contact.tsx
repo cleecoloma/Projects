701c87eb08683a379cc403ee3221a08a37246ff6
Your Name <104961833+cleecoloma@users.noreply.github.com>
2023-04-25 21:32:39 -0700

    added card contact form

--- electronic-store/src/pages/Contact.tsx
+++ electronic-store/src/pages/Contact.tsx
@@ -1,50 +1,34 @@
-import React from 'react';
-import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 
-function About() {
+function Contact() {
   return (
-    <Card style={{ width: '60rem' }} className='d-flex m-auto'>
-      <Card.Body>
-        <Card.Title>Welcome to our PC component e-commerce website!</Card.Title>
+    <>
+      <Card style={{ width: "38rem" }} className="m-auto p-3">
         <Card.Text>
-          <p>
-        We understand that building your own custom PC is important to many of
-        our customers, and that’s why we offer a comprehensive selection of
-        high-quality PC components from the world’s leading brands.
-      </p>
-      <p>
-        Our commitment to quality means that we only offer products that meet
-        our strict standards for performance, reliability, and value. Our
-        customers can choose from a wide variety of components, including CPUs,
-        motherboards, graphics cards, memory, storage, power supplies, cooling
-        systems, and much more.
-      </p>
-      <p>
-        Because we understand that every customer’s needs are unique, we provide
-        detailed product descriptions, specifications, and customer reviews to
-        ensure that you make an informed decision when making a purchase. Our
-        team of experts is always available to answer any questions you may
-        have, and we are passionate about helping our customers find the right
-        components to build the perfect PC.
-      </p>
-      <p>
-        Our customer service team is dedicated to providing the best possible
-        experience for every customer, and we are committed to delivering fast,
-        reliable shipping to ensure that your components arrive as quickly as
-        possible. We offer free delivery on all orders over a certain value, and
-        we make the checkout process as simple and streamlined as possible.
-      </p>
-      <p>
-        At our PC component e-commerce website, we believe that everyone
-        deserves access to the highest-quality PC components at an affordable
-        price. We are proud to be a trusted source for PC components, and we
-        look forward to helping you build the perfect PC.
-      </p>
+          <h3>Contact Us</h3>
+          <hr />
+          <p>Have a question about your order?</p>
+          <p>Please fill out the form below:</p>
+          <hr />
         </Card.Text>
-      </Card.Body>
-    </Card>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Name"
+          className="mb-3"
+        >
+          <Form.Control type="email" placeholder="name" />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Email Address" className="mb-3">
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Question">
+          <Form.Control as="textarea" rows={3} placeholder="question" style={{ height: "9rem" }} />
+        </FloatingLabel>
+      </Card>
+    </>
   );
 }
 
-export default About
+export default Contact;
